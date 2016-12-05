9cc3253b324bc50cdfac12304bc16840e595d6eb
Amandeep-saggu <aman007saggu@gmail.com>
2016-12-04 20:50:47 -0500

    commit 2
    Assignment 2 part 2

---
+++ Commit 2/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimeTrackerApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private TimeTrackerDBEntities1 db = new TimeTrackerDBEntities1();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+                    
+
+        
+        }
+    }
+}
