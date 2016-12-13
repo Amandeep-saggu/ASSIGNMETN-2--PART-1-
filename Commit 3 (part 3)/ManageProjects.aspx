367ea77545748b21a8b421e36b296f76c39a5f0b
Amandeep-saggu <aman007saggu@gmail.com>
2016-12-12 21:12:51 -0500

    Commit 3 (Manages project along with changes in database)
    commit 3(part 3)

---
+++ Commit 3 (part 3)/ManageProjects.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Website.Master" AutoEventWireup="true" CodeBehind="ManageProjects.aspx.cs" Inherits="TimeTrackerApp.WebForm5" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Manage Projects ( Add / Update / Delete Projects )</h1>
+    <asp:GridView CssClass="table table-responsive" ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="ProjectId"
+        OnRowDataBound="OnRowDataBound" OnRowEditing="OnRowEditing" OnRowCancelingEdit="OnRowCancelingEdit"
+        OnRowUpdating="OnRowUpdating" OnRowDeleting="OnRowDeleting" EmptyDataText="No records has been added.">
+        <Columns>
+            <asp:TemplateField HeaderText="Name" ItemStyle-Width="150">
+                <ItemTemplate>
+                    <asp:Label ID="lblTitle" runat="server" Text='<%# Eval("Title") %>'></asp:Label>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:TextBox ID="txtTitle" runat="server" Text='<%# Eval("Title") %>'></asp:TextBox>
+                </EditItemTemplate>
+            </asp:TemplateField>
+           
+            <asp:CommandField ButtonType="Link"  ControlStyle-CssClass="btn btn-primary"  ShowEditButton="true" ShowDeleteButton="true"
+                ItemStyle-Width="150" />
+        </Columns>
+    </asp:GridView>
+    <table class=" table table-responsive" border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+        <tr>
+            <td style="width: 150px">
+                Title:<br />
+                <asp:TextBox ID="txtTitle" CssClass="form-control" runat="server" Width="140" />
+            </td>
+           
+            <td style="width: 100px">
+                <asp:Button ID="btnAdd" CssClass="btn btn-primary"  runat="server" Text="Add" OnClick="Insert" />
+            </td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ Commit 3 (part 3)/ManageProjects.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TimeTrackerApp
+{
+    public partial class WebForm5 : System.Web.UI.Page
+    {
+        private TimeTrackerDBEntities db = new TimeTrackerDBEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            var listitemsrecord = (from x in db.TT_Projects select x).ToList<TT_Projects>();
+            GridView1.DataSource = listitemsrecord;
+            GridView1.DataBind();
+        }
+
+        protected void Insert(object sender, EventArgs e)
+        {
+            string Title = txtTitle.Text;
+            TT_Projects project = new TT_Projects();
+            project.Title = Title;
+            project.UserId = Convert.ToInt32(Session["User"].ToString());
+            
+            db.TT_Projects.Add(project);
+            db.SaveChanges();
+            this.BindGrid();
+        }
+
+        protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void OnRowCancelingEdit(object sender, EventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int projectId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string Title = (row.FindControl("txtTitle") as TextBox).Text;
+
+            TT_Projects project = db.TT_Projects.Single(proj => proj.ProjectID == projectId);
+            //Field which will be update
+            project.Title = Title;
+            // executes the appropriate commands to implement the changes to the database
+            db.SaveChanges();
+            
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
+            {
+                (e.Row.Cells[1].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+            }
+        }
+
+        protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int projectid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            TT_Projects project = db.TT_Projects.Single(proj => proj.ProjectID == projectid);
+
+            db.TT_Projects.Remove(project);
+            db.SaveChanges();  
+
+            this.BindGrid();
+        }
+    }
+}
